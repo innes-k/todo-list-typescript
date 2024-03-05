9a64a116e5401904542b1cf86b01923ec6d17b6f
innes-k <innes.kwak@gmail.com>
2024-03-05 16:40:44 +0900

    feat: InputBox '추가' 동작 구현 (type 생성 필요)

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -1,5 +1,48 @@
-const InputBox = () => {
-  return <div>InputBox</div>;
-};
+import * as St from "./styles/InputBox.styles";
+
+// input Box 컴포넌트 (상단의 제목, 내용, 추가하기)
+function InputBox({ setTodos }) {
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const body = e.target.body.value;
+    const deadline = e.target.trip.value;
+
+    setTodos((todo) => [
+      ...todo,
+      {
+        id: Date.now(),
+        title: title,
+        body: body,
+        isDone: false,
+        deadline: deadline,
+      },
+    ]);
+    e.target.reset();
+  };
+  return (
+    <St.Container>
+      <St.Title onSubmit={onSubmitHandler}>
+        <St.TitleInput name="title" placeholder="제목" />
+        <St.TitleInput name="body" placeholder="내용" />
+        <St.Time>
+          <label htmlFor="start">마감일 :</label>
+          <St.TimeInput
+            type="date"
+            id="start"
+            name="trip"
+            min="2024-01-01"
+            max="2026-12-31"
+          />
+        </St.Time>
+        <div className="addBtn">
+          <St.AddBtn type="submit" className="btn text-white">
+            추가하기
+          </St.AddBtn>
+        </div>
+      </St.Title>
+    </St.Container>
+  );
+}
 
 export default InputBox;
---
+++ src/components/styles/InputBox.styles.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Title = styled.form`
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  background-color: white;
+  margin-bottom: 50px;
+  border-radius: 30px;
+  border: 2px solid rgb(135, 206, 235);
+`;
+
+export const TitleInput = styled.input`
+  width: 300px;
+  height: 40px;
+  margin: 10px auto 10px auto;
+`;
+
+export const Time = styled.time`
+  margin-bottom: 10px;
+`;
+
+export const TimeInput = styled.input`
+  width: 250px;
+  height: 30px;
+  text-align: center;
+  margin: 10px auto 0px auto;
+`;
+
+export const AddBtn = styled.button`
+  width: 300px;
+  height: 40px;
+  margin: 10px;
+  background-color: rgb(135, 206, 235);
+
+  color: white;
+  font-size: 15px;
+  border: 0px;
+  border-radius: 10px;
+`;
