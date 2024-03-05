608962d57197e51a3c5ead104baf497629ba3e20
innes-k <innes.kwak@gmail.com>
2024-03-05 16:18:41 +0900

    feat: Header 완성

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Title>
+      <span>☁️ Welcome to To Do List! ⛅️</span>
+    </Title>
+  );
 };
 
 export default Header;
+
+export const Title = styled.div`
+  font-size: 50px;
+  font-weight: bold;
+  text-align: center;
+  padding: 30px;
+`;
