1a4708ffb12c4c0fcc8a465829d86840689faed6
innes-k <innes.kwak@gmail.com>
2024-03-05 19:43:29 +0900

    feat: props-drilling으로 ts 구현하기

--- src/redux/config/configStore.tsx
+++ src/redux/config/configStore.tsx
@@ -1,5 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { todoReducer } from "../modules/todoSlice";
 
-export const store = configureStore({
-  reducer: {},
-});
+// export const store = configureStore({
+//   reducer: { todoReducer },
+// });
