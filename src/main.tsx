130ac936846b036bf43e20a086aa35db38734001
innes-k <innes.kwak@gmail.com>
2024-03-06 21:06:08 +0900

    feat: QueryClientProvider 설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import GlobalStyle from "./styles/GlobalStyle.ts";
-import { Provider } from "react-redux";
-import { store } from "./redux/config/configStore.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { Provider } from "react-redux";
+// import { store } from "./redux/config/configStore.tsx";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      {/* <Provider store={store}> */}
       <GlobalStyle />
       <App />
-    </Provider>
+      {/* </Provider> */}
+    </QueryClientProvider>
   </React.StrictMode>
 );
