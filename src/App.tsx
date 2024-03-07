d4a2788d1e74047cc40dc5546a2cede6b30402fa
innes-k <innes.kwak@gmail.com>
2024-03-07 15:48:34 +0900

    feat: router provider설정, home path 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import Home from "./pages/Home";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Routes";
 
 const App = () => {
   return (
     <>
-      <Home />
+      <RouterProvider router={router} />
     </>
   );
 };
